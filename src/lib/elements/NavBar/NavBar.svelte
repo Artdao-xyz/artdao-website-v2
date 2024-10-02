4801608772bcf8fcb42935a0dc2f5dac844c9dbb
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-02 16:19:40 -0300

    feat: :sparkles: Modify navbar

--- src/lib/elements/NavBar/NavBar.svelte
+++ src/lib/elements/NavBar/NavBar.svelte
@@ -14,17 +14,16 @@
 </script>
 
 <div
-	class="w-[75rem] macBook:w-[85.1875rem] rounded-[6.25rem] h-[1rem] flex flex-row items-center z-50 sticky top-[1.95%] mx-auto"
+	class="w-100dvw rounded-[6.25rem] h-[1rem] flex flex-row items-center z-50 sticky top-[1.95%] mx-10 gap-2.5"
 >
 	{#each navItems as navItem, i}
 		<a
 			data-sveltekit-noscroll
 			href={`#${navItem.route}`}
-			class="rounded-[6.25rem] font-robotoMono text-[0.75rem] font-medium tracking-[0.075rem] !opacity-100 {navItem.selected
-				? 'dark-gradient-nav text-color-white font-semibold'
-				: 'gray-gradient-nav text-color-white'} capitalize h-[1rem]
-                flex flex-row items-center justify-end py-[0.75rem] pr-[2.5rem] pl-[3.75rem]
-               {i !== 0 && 'ml-[-3.125rem]'} "
+			class="rounded-[6.25rem] font-robotoMono text-[0.75rem] font-medium tracking-[0.075rem] !opacity-100 text-color-white {navItem.selected
+				? 'nav-gradient-selected justify-center font-semibold border border-color-dark'
+				: 'nav-gradient-unselected justify-end'} capitalize h-[1rem]
+                flex flex-row items-center py-[0.75rem] pr-[2.5rem] pl-[3.75rem] transition delay-75 duration-700 ease-in-out transform"
 			style="width: {percentage}%; z-index: -{i};"
 			on:click={() => handleOnClick(i)}
 		>
