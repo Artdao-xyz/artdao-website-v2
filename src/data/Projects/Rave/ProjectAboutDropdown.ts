51f41137a4d87389b13eb857a00efd62e1206583
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-07 18:53:14 -0300

    feat: :sparkles: Updated Rave links

--- src/data/Projects/Rave/ProjectAboutDropdown.ts
+++ src/data/Projects/Rave/ProjectAboutDropdown.ts
@@ -19,24 +19,11 @@ import yulia from '$lib/assets/images/projects/rave/imgNavigator/yulia.png';
 import zak from '$lib/assets/images/projects/rave/imgNavigator/zak.png';
 import type { IAboutDropdown } from '$lib/elements/AboutDropdown/interfaces';
 
-export const raveMemoriesImages = [
-	Artdao_Photos93,
-	Artdao_Photos6,
-	Artdao_Photos4,
-	_DSC3824_4,
-	_DSC3824_3,
-	_DSC3824_1,
-	_DSC2417,
-	_DSC2393,
-	_DSC2076,
-	_DSC1779
-];
-
 export const raveAboutDropdopwnItems: IAboutDropdown[] = [
 	{
 		name: 'Meteor Sky',
 		artist: 'Yuliya Liberda',
-		link: '#',
+		link: 'https://zora.co/collect/zora:0x2a5299a3d0542869f3d9ad57937ba6e6149e14ec/3',
 		about:
 			'An ethereal landscape inspired by the meteor that left the dark sky blu on may 19th in Portugal and Spain and what it represents: the awe, the fear, the uniqueness, the fascination toward the unknown.',
 		image: yulia
@@ -44,6 +31,7 @@ export const raveAboutDropdopwnItems: IAboutDropdown[] = [
 	{
 		name: 'UNDETECTED 0.1',
 		artist: 'fwvn',
+		link: 'https://zora.co/collect/zora:0x2a5299a3d0542869f3d9ad57937ba6e6149e14ec/19',
 		about:
 			'Some of them say that we’re sick, we’re crazy. And some of them think that we are the most gorgeous, special things on Earth.',
 		image: dodgedABullet
@@ -51,11 +39,13 @@ export const raveAboutDropdopwnItems: IAboutDropdown[] = [
 	{
 		name: 'Fear of Death',
 		artist: 'Zak Krevitt',
+		link: 'https://zora.co/collect/zora:0x2a5299a3d0542869f3d9ad57937ba6e6149e14ec/9',
 		image: zak
 	},
 	{
 		name: 'Las Raras',
 		artist: 'Daira',
+		link: 'https://zora.co/collect/zora:0x2a5299a3d0542869f3d9ad57937ba6e6149e14ec/11',
 		about:
 			'(𝙏𝙝𝙚 𝙛𝙞𝙧𝙨𝙩 𝙡𝙞𝙣𝙚 𝙤𝙛 𝙚𝙭𝙥𝙪𝙡𝙨𝙞𝙤𝙣 𝙛𝙧𝙤𝙢 𝙩𝙝𝙚 𝙡𝙜𝙗𝙩𝙦+ 𝙘𝙤𝙢𝙢𝙪𝙣𝙞𝙩𝙮 𝙞𝙨 𝙩𝙝𝙚 𝙛𝙖𝙢𝙞𝙡𝙮 𝙝𝙤𝙢𝙚.) Under the intoxicating burst of fantasy and the deafening sound of the bass Las Raras (The Rare Ones) find a place of belonging, we sediment counterculture, devising mythologies, and practicing our own rituals. The party enables our existence through enjoyment, we unleash sensuality beyond the morality of good manners. In fact, we shit on good manners and modesty, we built a monstrous and strident family functional to its members and not to the cis-tem. 𝗧𝗵𝗲 𝗵𝗮𝗿𝗱𝗻𝗲𝘀𝘀 𝗰𝗼𝗻𝘁𝗮𝗶𝗻𝗶𝗻𝗴 𝘁𝗵𝗲 𝗺𝗼𝘀𝘁 𝘁𝗲𝗻𝗱𝗲𝗿 𝘀𝗼𝗳𝘁𝗻𝗲𝘀𝘀. Now in the blinding brightness of the sun, Las Raras gather around a glass table to continue our histrionic exchanges until we deplete the substances that alienate us a little from the hostility that surrounds us. And, if as the days go by, the post-party anguish affects us, we will know how to accompany and take care of ourselves in our vulnerability within our chosen family until the next time ecstasy summons us. And even if they don’t want us, we will continue to be scandalous occupying all the spaces we want to because Las Raras don’t know about fear.',
 
@@ -64,16 +54,19 @@ export const raveAboutDropdopwnItems: IAboutDropdown[] = [
 	{
 		name: 'Facetune_22-05-2024-23-27-09',
 		artist: '420 Bombshell',
+		link: 'https://zora.co/collect/zora:0x2a5299a3d0542869f3d9ad57937ba6e6149e14ec/10',
 		image: bombshell
 	},
 	{
 		name: 'Delirio',
 		artist: 'Cymoonv',
+		link: 'https://zora.co/collect/zora:0x2a5299a3d0542869f3d9ad57937ba6e6149e14ec/17',
 		image: cymoon
 	},
 	{
 		name: 'Gagged',
 		artist: 'Duartsy',
+		link: 'https://zora.co/collect/zora:0x2a5299a3d0542869f3d9ad57937ba6e6149e14ec/4',
 		about:
 			'A body that exists solely as a vessel, endlessly indulging in euphoria through external hyper-stimulation.',
 		image: duartsy
@@ -81,6 +74,7 @@ export const raveAboutDropdopwnItems: IAboutDropdown[] = [
 	{
 		name: 'PROMETHEUS',
 		artist: 'enco',
+		link: 'https://zora.co/collect/zora:0x2a5299a3d0542869f3d9ad57937ba6e6149e14ec/12',
 		about: '3D',
 		image: enco
 	}
--- src/lib/elements/AboutDropdown/AboutDropdown.svelte
+++ src/lib/elements/AboutDropdown/AboutDropdown.svelte
@@ -35,7 +35,7 @@
 			id="big"
 			in:send={{ key: 'big' }}
 			out:receive={{ key: 'small' }}
-			class="flex flex-col gap-[0.5rem] w-[20rem] h-[16.6875rem] rounded-20 gray-gradient p-5 pb-10 absolute z-40 top-0 mt-[2.5%]"
+			class="flex flex-col gap-[0.5rem] w-[20rem] min-h-[16.6875rem] h-fit rounded-20 gray-gradient p-5 pb-10 absolute z-40 top-0 mt-[2.5%]"
 		>
 			<h1 class="font-clash text-[2.5rem] leading-[3.125rem] capitalize self-start">
 				{aboutDropdown.name}
@@ -56,12 +56,14 @@
 				class="flex items-center justify-between font-robotoMono font-light text-sm leading-[0.8125rem] tracking-[0.075rem] py-[9px] px-[14px]"
 			>
 				<p class="font-clash text-[0.75rem]">Link</p>
-				<a href={aboutDropdown.link}><img src={linkIcon} alt="Link" class="w-[1.25rem]" /></a>
+				<a href={aboutDropdown.link} target="_blank" rel="noopener noreferrer"
+					><img src={linkIcon} alt="Link" class="w-[1.25rem]" /></a
+				>
 			</div>
 
 			<img src={dottedLine} alt="Dotted Line" />
 
-			<div class="h-full w-full flex justify-center items-end">
+			<div class="h-full w-full flex justify-center items-end mt-5">
 				<img src={menuLine} alt="Menu Line" class="w-[8.25rem] justify-center" />
 			</div>
 		</div>
@@ -91,7 +93,7 @@
 				<img src={menuLine} alt="Menu Line" class="w-[8.25rem] justify-center" />
 			</div>
 		</div>
-	{:else if aboutDropdown.about && aboutDropdown.artist}
+	{:else if aboutDropdown.about && aboutDropdown.artist && aboutDropdown.link}
 		<div
 			id="big"
 			in:send={{ key: 'big' }}
@@ -109,6 +111,17 @@
 			>
 				<p class="font-clash text-[0.75rem]">Artist</p>
 				<p class="capitalize text-[0.75rem]">{aboutDropdown.artist}</p>
+			</div>
+
+			<img src={dottedLine} alt="Dotted Line" />
+
+			<div
+				class="flex items-center justify-between font-robotoMono font-light text-sm leading-[0.8125rem] tracking-[0.075rem] py-[9px] px-[14px]"
+			>
+				<p class="font-clash text-[0.75rem]">Link</p>
+				<a href={aboutDropdown.link} target="_blank" rel="noopener noreferrer"
+					><img src={linkIcon} alt="Link" class="w-[1.25rem]" /></a
+				>
 			</div>
 
 			<img src={dottedLine} alt="Dotted Line" />
