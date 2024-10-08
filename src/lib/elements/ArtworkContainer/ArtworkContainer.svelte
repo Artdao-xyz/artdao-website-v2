1c1562f5ba28b23a249244dd76083589a0df738a
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-08 20:41:46 -0300

    feat: :sparkles: Fix grid

--- src/lib/elements/ArtworkContainer/ArtworkContainer.svelte
+++ src/lib/elements/ArtworkContainer/ArtworkContainer.svelte
@@ -7,13 +7,9 @@
 </script>
 
 <div
-	class="h-full w-auto p-5 bg-color-white rounded-30 border border-color-black flex flex-col gap-5 bigScreen:gap-2.5 flex-shrink-0"
+	class="h-full w-auto p-5 bg-color-white rounded-30 border border-color-black flex flex-col flex-shrink-0 flex-grow-0 justify-between"
 >
-	<img
-		src={image}
-		alt="Gallery"
-		class="h-[80%] macBook:h-[84%] laptopL:h-[80%] bigScreen:h-[85%]"
-	/>
+	<img src={image} alt="Gallery" class="h-[83%]" />
 	{#if showDetails}
 		<div
 			class="flex flex-col text-color-black font-robotoMono text-[0.625rem] bigScreen:text-[1rem] font-semibold leading-4 bigScreen:leading-6"
