6c180601fcc68a5e28d9922965def3e1024a419c
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-09 17:12:36 -0300

    feat: :sparkles: Ecologies portrait

--- src/data/Projects/EcologiesOfCode/ProjectIntro.ts
+++ src/data/Projects/EcologiesOfCode/ProjectIntro.ts
@@ -1,6 +1,9 @@
+import bgImage from '$lib/assets/images/projects/ecologiesOfCode/EcologiesOfCode_IMG 1-min.png';
+
 export const ecologiesOfCodeProject = {
 	name: 'ecologies of code',
 	description: [
 		'The tools of digital technologies open up new pathways to the construction of digital landscapes as symbiotic environments that place the viewer in an immersive relationship with the work of art. Exploring different forms of interactivity and feedback loops between systems of data drawn from nature. '
-	]
+	],
+	bgImage: bgImage
 };
