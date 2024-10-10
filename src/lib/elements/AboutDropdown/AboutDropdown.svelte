f0e1849b99e395de9de1d3e0a72f5fcb76465777
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-10 18:00:06 -0300

    feat: :sparkles: Fixes dropdown

--- src/lib/elements/AboutDropdown/AboutDropdown.svelte
+++ src/lib/elements/AboutDropdown/AboutDropdown.svelte
@@ -2,6 +2,7 @@
 	import linkIcon from '$lib/assets/images/about-dropdown-link-icon.png';
 	import dottedLine from '$lib/assets/images/dotted-line-about-dropdown.png';
 	import infoCircle from '$lib/assets/images/Ellipse 30.png';
+	import infoCircleWhite from '$lib/assets/images/Ellipse-white.png';
 	import { slide } from 'svelte/transition';
 	import { type IAboutDropdown } from './interfaces';
 
@@ -19,23 +20,29 @@
 		class="flex flex-col gap-2.5 w-[20rem] rounded-20 gray-gradient p-5 absolute z-40 top-0 mt-[2.5%]"
 	>
 		<div class="flex flex-col items-start gap-[0.3125rem]">
-			<div class="flex flex-row justify-between w-full">
-				<h1 class="font-clash text-[12px] leading-[0.875rem] capitalize self-start">
-					{aboutDropdown.name}
-				</h1>
-
-				<div class="flex flex-row gap-1 items-center">
-					<p class="font-robotoMono text-[0.5rem] leading-[1rem] tracking-[0.05rem]">INFO</p>
-					<img src={infoCircle} alt="info" class="w-[0.5rem] h-[0.5rem]" />
-				</div>
-			</div>
+			<h1
+				class="font-clash text-[0.875rem] leading-[0.875rem] tracking-[1.6px] capitalize self-start"
+			>
+				{aboutDropdown.name}
+			</h1>
 
 			{#if aboutDropdown.artist}
-				<h1
-					class="font-robotoMono text-[0.625rem] leading-[1rem] tracking-[0.0625rem] capitalize self-start"
-				>
-					{aboutDropdown.artist}
-				</h1>
+				<div class="flex flex-row justify-between w-full">
+					<h1
+						class="font-robotoMono text-[12px] leading-[1rem] tracking-[0.0625rem] capitalize self-start"
+					>
+						{aboutDropdown.artist}
+					</h1>
+
+					<div class="flex flex-row gap-1 items-center">
+						<p class="font-robotoMono text-[10px] leading-[1rem] tracking-[0.05rem]">INFO</p>
+						<img
+							src={!visible ? infoCircle : infoCircleWhite}
+							alt="info"
+							class="w-[0.5rem] h-[0.5rem]"
+						/>
+					</div>
+				</div>
 			{/if}
 		</div>
 
@@ -46,13 +53,13 @@
 				{#if aboutDropdown.date}
 					<div class="flex flex-row justify-between w-full">
 						<p
-							class="font-robotoMono text-[0.625rem] leading-[1.125rem] tracking-[0.0625rem] capitalize self-start"
+							class="font-robotoMono text-[12px] leading-[1.125rem] tracking-[0.0625rem] capitalize self-start"
 						>
 							Date
 						</p>
 
 						<p
-							class="font-robotoMono text-[0.625rem] leading-[1.125rem] tracking-[0.0625rem] capitalize self-start"
+							class="font-robotoMono text-[12px] leading-[1.125rem] tracking-[0.0625rem] capitalize self-start"
 						>
 							{aboutDropdown.date}
 						</p>
@@ -64,13 +71,13 @@
 				{#if aboutDropdown.location}
 					<div class="flex flex-row justify-between w-full">
 						<p
-							class="font-robotoMono text-[0.625rem] leading-[1.125rem] tracking-[0.0625rem] capitalize self-start"
+							class="font-robotoMono text-[12px] leading-[1.125rem] tracking-[0.0625rem] capitalize self-start"
 						>
 							Location
 						</p>
 
 						<p
-							class="font-robotoMono text-[0.625rem] leading-[1.125rem] tracking-[0.0625rem] capitalize self-start"
+							class="font-robotoMono text-[12px] leading-[1.125rem] tracking-[0.0625rem] capitalize self-start"
 						>
 							{aboutDropdown.location}
 						</p>
@@ -80,9 +87,7 @@
 				{/if}
 
 				<div class="flex items-center justify-between font-light text-sm h-[1.125rem]">
-					<p class="font-robotoMono text-[0.625rem] leading-[1.125rem] tracking-[0.0625rem]">
-						Link
-					</p>
+					<p class="font-robotoMono text-[12px] leading-[1.125rem] tracking-[0.0625rem]">Link</p>
 					<a href={aboutDropdown.link} target="_blank" rel="noopener noreferrer"
 						><img src={linkIcon} alt="Link" class="w-[1.25rem]" /></a
 					>
@@ -91,7 +96,7 @@
 				<img src={dottedLine} alt="Dotted Line" />
 
 				<div
-					class="flex flex-col gap-[0.5rem] justify-start items-start font-robotoMono font-light text-[0.625rem] leading-[1.125rem] tracking-[0.0625rem] max-h-[25.5rem] {!visible
+					class="flex flex-col gap-[0.5rem] justify-start items-start font-robotoMono font-light text-[12px] leading-[1.125rem] tracking-[0.0625rem] max-h-[25.5rem] {!visible
 						? 'overflow-y-auto'
 						: ''}"
 				>
