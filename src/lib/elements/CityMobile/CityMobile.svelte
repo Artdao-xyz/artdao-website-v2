def9608c9d476eaa6f7c8da89f924a27ae4083b5
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-27 00:19:26 -0300

    feat: :sparkles: Working on mobile map

---
+++ src/lib/elements/CityMobile/CityMobile.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import type { IMapEvent, IMapLocation } from '../../../data/Map/MapData';
+
+	export let mapLocation: IMapLocation;
+	export let eventToShow: IMapEvent | undefined;
+	let isSelected = false;
+
+	const toggleSelected = () => {
+		isSelected = !isSelected;
+	};
+
+	const handleOnClick = (event: IMapEvent) => {
+		eventToShow = event;
+	};
+</script>
+
+<button
+	on:click={toggleSelected}
+	class="relative flex flex-row {!isSelected
+		? 'gray-gradient'
+		: 'dark-gradient'} items-center justify-center rounded-40 py-[15px] px-[17px] w-full h-[5.4375rem] {!isSelected
+		? 'unselected'
+		: 'selected'}"
+>
+	<p
+		class="w-full h-full text-color-white font-robotoMono text-[18px] font-medium tracking-[0.234px] uppercase flex items-center justify-center"
+	>
+		{mapLocation.location}
+	</p>
+
+	<div class="w-[0.75rem] h-[0.75rem] bg-color-white rounded-[100px]" />
+</button>
+
+{#if isSelected}
+	<div class="flex flex-col justify-center items-center gap-[5px] w-full">
+		{#each mapLocation.events as event}
+			<button
+				on:click={() => handleOnClick(event)}
+				class="flex flex-rowitems-center justify-center rounded-40 w-full h-[47px] gray-gradient"
+			>
+				<p
+					class="font-neue text-[20px] tracking-[0.0163rem] font-semibold uppercase flex justify-center items-center leading-[47px]"
+				>
+					{event.title}
+				</p>
+			</button>
+		{/each}
+	</div>
+{/if}
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,26 +1,45 @@
 <script lang="ts">
 	import mapBg from '$lib/assets/images/map-background.png';
 	import City from '$lib/elements/City/City.svelte';
+	import CityMobile from '$lib/elements/CityMobile/CityMobile.svelte';
 	import EventData from '$lib/elements/EventData/EventData.svelte';
 	import SectionContainer from '$lib/elements/SectionContainer/SectionContainer.svelte';
 	import { EColorVariant } from '../../constants/enums';
 	import { mapData, type IMapEvent } from '../../data/Map/MapData';
 
 	let eventToShow: IMapEvent | undefined = undefined;
+	let width: number;
+	let cityToShow: string | undefined;
 </script>
 
+<svelte:window bind:innerWidth={width} />
 <SectionContainer colorVariant={EColorVariant.BLACK}>
 	<div class="h-full flex justify-center items-center relative w-full">
-		{#if !eventToShow}
-			<img src={mapBg} alt="map" class="h-full" />
-			<City mapLocation={mapData[0]} top="27" left="44.5" dotOnLeft={false} bind:eventToShow />
-			<City mapLocation={mapData[1]} top="34.5" left="49" bind:eventToShow />
-			<City mapLocation={mapData[2]} top="40" left="24" bind:eventToShow />
-			<City mapLocation={mapData[3]} top="56.5" left="30.2" bind:eventToShow />
-			<City mapLocation={mapData[4]} top="67" left="36" bind:eventToShow />
-			<City mapLocation={mapData[5]} top="73" left="34" showOnTop bind:eventToShow />
+		{#if width > 768}
+			{#if !eventToShow}
+				<img src={mapBg} alt="map" class="h-full" />
+				<City mapLocation={mapData[0]} top="27" left="44.5" dotOnLeft={false} bind:eventToShow />
+				<City mapLocation={mapData[1]} top="34.5" left="49" bind:eventToShow />
+				<City mapLocation={mapData[2]} top="40" left="24" bind:eventToShow />
+				<City mapLocation={mapData[3]} top="56.5" left="30.2" bind:eventToShow />
+				<City mapLocation={mapData[4]} top="67" left="36" bind:eventToShow />
+				<City mapLocation={mapData[5]} top="73" left="34" showOnTop bind:eventToShow />
+			{:else}
+				<EventData bind:eventToShow />
+			{/if}
+		{:else if !eventToShow}
+			<div
+				class="w-full h-full py-[42px] px-[25px] flex flex-col gap-[15px] items-center justify-center bg-color-gray rounded-20"
+			>
+				<CityMobile mapLocation={mapData[0]} bind:eventToShow />
+				<CityMobile mapLocation={mapData[1]} bind:eventToShow />
+				<CityMobile mapLocation={mapData[2]} bind:eventToShow />
+				<CityMobile mapLocation={mapData[3]} bind:eventToShow />
+				<CityMobile mapLocation={mapData[4]} bind:eventToShow />
+				<CityMobile mapLocation={mapData[5]} bind:eventToShow />
+			</div>
 		{:else}
-			<EventData bind:eventToShow />
+			<div>EVENT TO SHOW</div>
 		{/if}
 	</div>
 </SectionContainer>
