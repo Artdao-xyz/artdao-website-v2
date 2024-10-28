8323359f0078cabc483e1c70300ee312d476edce
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-28 07:44:36 -0300

    feat: :sparkles: Fix video card

--- src/lib/elements/VideoCard/VideoCard.svelte
+++ src/lib/elements/VideoCard/VideoCard.svelte
@@ -14,7 +14,7 @@
 		videoProject.size === 'rectangle'
 			? 'w-full bigScreen:max-w-[85%] laptopL:max-w-[80%] macBook:max-h-[800px] macBook:w-[80rem] macBook:max-w-full bigScreen:!h-[90%] max-h-[360px] sm:max-h-[40rem] sm:w-[45rem]'
 			: videoProject.size === 'square'
-				? 'w-full sm:w-[700px] max-h-[420px] sm:h-[800px] sm:max-h-[800px] '
+				? 'w-full sm:w-[600px] max-h-[420px] sm:h-[800px] sm:max-h-[600px] bigScreen:w-[800px] bigScreen:max-h-[800px]'
 				: 'max-h-[500px] sm:h-full w-full max-w-[350px] sm:max-h-[800px] sm:max-w-[450px]';
 </script>
 
