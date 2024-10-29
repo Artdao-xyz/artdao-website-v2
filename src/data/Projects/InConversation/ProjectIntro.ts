aefd1e5392bf4e91c5f102a9285073aa753b4998
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-29 19:14:06 -0300

    feat: :sparkles: Operator title

--- src/data/Projects/InConversation/ProjectIntro.ts
+++ src/data/Projects/InConversation/ProjectIntro.ts
@@ -2,7 +2,7 @@ import bgImage from '$lib/assets/images/projects/inConversation/Operator_Cover 4
 import bgImageMobile from '$lib/assets/images/projects/inConversation/Operator_Cover_Mobile 2-min.png';
 
 export const inConversationProjectIntro = {
-	name: 'in conversation with operator',
+	name: 'interview with operator',
 	description: [
 		"As part of the Intertwined’s talks “Art in the Age of Technology” during NFC Lisbon, a compelling dialogue unfolded between Jeff Davis and the artistic duo Operator: Ania Catherine and Dejha. Their conversation explored various themes, from the collective's creative process to the evolution of technology, its potential for artist practice and connecting technology with human bodies and the expanding horizons of AI."
 	],
