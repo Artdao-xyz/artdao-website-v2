27412df7d352229ab987a3e6c98d8860f901886b
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-29 19:31:13 -0300

    feat: :sparkles: Fix mobile home position

--- src/lib/elements/HomeIcon/HomeIcon.svelte
+++ src/lib/elements/HomeIcon/HomeIcon.svelte
@@ -9,6 +9,8 @@
 	<img
 		src={homeIcon}
 		alt="Home Icon"
-		class="fixed bottom-[5%] {isHome ? 'right-[2%]' : 'right-[3%]'} w-[4.6875rem] hidden sm:block"
+		class="fixed bottom-[5%] {isHome
+			? 'right-[2.15%]'
+			: 'right-[3%]'} w-[4.6875rem] hidden sm:block"
 	/>
 </a>
