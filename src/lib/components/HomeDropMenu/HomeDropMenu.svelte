a907a6d3b144399efe94dea8ef80cc74b2c3137d
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-29 19:38:19 -0300

    feat: :sparkles: Fix white toggle

--- src/lib/components/HomeDropMenu/HomeDropMenu.svelte
+++ src/lib/components/HomeDropMenu/HomeDropMenu.svelte
@@ -28,7 +28,7 @@
 	style={`background-image: url("${bgImage}"); background-size: cover; background-position: center;`}
 	class="w-full h-[100dvh] p-2 relative snap-start"
 >
-	<a href="/drops" class="hidden sm:block">
+	<a href="/" class="hidden sm:block">
 		<img src={whiteToggle} alt="Go to Home" class="w-[5.9063rem] cursor-pointer mb-[-0.5rem]" />
 	</a>
 
