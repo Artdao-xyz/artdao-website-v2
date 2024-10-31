1d6a1276a8e3c542c8f6a8688e8bd7209a4c89e2
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-31 11:31:31 -0300

    feat: :sparkles: Added video links

--- src/data/Projects/Artifice/ProjectVideo.ts
+++ src/data/Projects/Artifice/ProjectVideo.ts
@@ -5,7 +5,8 @@ export const furnitureVideo: IVideoProject[] = [
 		name: 'Through what fictions do we discover our human nature?',
 		artist: 'Artist',
 		year: '2024',
-		videoUrl: '',
+		videoUrl:
+			'https://storage.googleapis.com/videoszine/Artifice/ARTIFICE%20VIDEOS/Introartifice.mp4#t=0.1',
 		size: 'rectangle'
 	}
 ];
@@ -15,7 +16,8 @@ export const afterEventVideo: IVideoProject[] = [
 		name: 'After the event',
 		artist: 'Artist',
 		year: '2024',
-		videoUrl: '',
+		videoUrl:
+			'https://storage.googleapis.com/videoszine/Artifice/ARTIFICE%20VIDEOS/Artifice%20Offline04%20(1).mp4#t=0.1',
 		size: 'rectangle'
 	}
 ];
