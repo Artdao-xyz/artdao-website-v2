5b1b2d138c6caf1890f77c280caa85010c099979
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-10-31 10:29:48 -0300

    feat: :sparkles: Hover gradient to home menu

--- src/lib/elements/HomeTop/HomeTop.svelte
+++ src/lib/elements/HomeTop/HomeTop.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 	export let logo: string;
 	export let homeParagraphs: string[];
+
+	let m = { x: 0, y: 0 };
+
+	function handleMousemove(event: any) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+
+	$: x = m.x;
+	$: y = m.y;
+
+	let width: number;
+	let height: number;
 </script>
 
-<div
-	class="flex flex-col gap-[1.875rem] w-full py-[1.875rem] px-5 sm:px-10 bg-color-dark rounded-20 h-full sm:h-fit"
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+<button
+	style={`background: radial-gradient(at ${(m.x * 1000) / width}% ${(m.y * 1000) / height}%, #A6A6A6, #313431)`}
+	on:mousemove={handleMousemove}
+	class="flex flex-col gap-[1.875rem] w-full py-[1.875rem] px-5 sm:px-10 rounded-20 h-full sm:h-fit"
 >
 	<img src={logo} alt="ArtDao Logo" class="w-[13.5625rem] h-[3.0931rem]" />
 	<div
@@ -14,4 +30,4 @@
 			<p>{homeParagraph}</p>
 		{/each}
 	</div>
-</div>
+</button>
