cc2f30125d3562d3d2a0d37031036e3b5b6e2402
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-11-01 09:54:14 -0300

    feat: :sparkles: Modified home images desktop and mobile

--- src/data/HomeImgLayout.ts
+++ src/data/HomeImgLayout.ts
@@ -1,12 +1,21 @@
-import digitalMatter from '$lib/assets/images/DIgitalMatter_Home_Square 1-min.png';
-import ecologies from '$lib/assets/images/IMG Home Sizes-min.png';
-import artifice from '$lib/assets/images/projects/artifice/Artifice_Cover_Home 1-min.png';
-import inConversation from '$lib/assets/images/projects/inConversation/IMG Home Sizes 1.png';
-import inherent from '$lib/assets/images/projects/inherentInstability/InherentInstability_Home_Square_1.png';
-import intertwined from '$lib/assets/images/projects/intertwined/Interwined_Home_Vertical 1-min.png';
-import nonPlaces from '$lib/assets/images/projects/nonPlaces/nonPlacesHome-min.png';
-import orb from '$lib/assets/images/projects/orb/Orb_Home_Square 1-min.png';
-import rave from '$lib/assets/images/RAVE_Home_Horizontal 1-min.png';
+import artifice from '$lib/assets/images/projects/desktop-home/artifice-min.png';
+import ecologies from '$lib/assets/images/projects/desktop-home/ecologies-min.png';
+import inherent from '$lib/assets/images/projects/desktop-home/inherent-min.png';
+import intertwined from '$lib/assets/images/projects/desktop-home/interwined-min.png';
+import digitalMatter from '$lib/assets/images/projects/desktop-home/matter-min.png';
+import nonPlaces from '$lib/assets/images/projects/desktop-home/nonplaces-min.png';
+import inConversation from '$lib/assets/images/projects/desktop-home/operator-min.png';
+import orb from '$lib/assets/images/projects/desktop-home/orb-min.png';
+import rave from '$lib/assets/images/projects/desktop-home/rave-min.png';
+import artificeMobile from '$lib/assets/images/projects/mobile-home/Artifice-min.png';
+import ecologiesMobile from '$lib/assets/images/projects/mobile-home/Ecologies-min.png';
+import inherentMobile from '$lib/assets/images/projects/mobile-home/Instability-min.png';
+import interwinedMobile from '$lib/assets/images/projects/mobile-home/Interwined-min.png';
+import matterMobile from '$lib/assets/images/projects/mobile-home/Matter-min.png';
+import nonplacesMobile from '$lib/assets/images/projects/mobile-home/Non-min.png';
+import operatorMobile from '$lib/assets/images/projects/mobile-home/Operator-min.png';
+import orbMobile from '$lib/assets/images/projects/mobile-home/Orb-min.png';
+import raveMobile from '$lib/assets/images/projects/mobile-home/Rave-min.png';
 
 import {
 	ARTIFICE,
@@ -21,13 +30,25 @@ import {
 } from '../constants/routes';
 
 export const homeImages = [
-	{ imageUrl: digitalMatter, route: DIGITAL_MATTER },
 	{ imageUrl: inherent, route: INHERENT },
-	{ imageUrl: rave, route: RAVE },
+	{ imageUrl: digitalMatter, route: DIGITAL_MATTER },
 	{ imageUrl: ecologies, route: ECOLOGIES_OF_CODE },
-	{ imageUrl: intertwined, route: INTERTWINED },
 	{ imageUrl: artifice, route: ARTIFICE },
+	{ imageUrl: rave, route: RAVE },
 	{ imageUrl: orb, route: ORB },
 	{ imageUrl: nonPlaces, route: NON_PLACES },
-	{ imageUrl: inConversation, route: IN_CONVERSATION }
+	{ imageUrl: inConversation, route: IN_CONVERSATION },
+	{ imageUrl: intertwined, route: INTERTWINED }
+];
+
+export const homeImagesMobile = [
+	{ imageUrl: inherentMobile, route: INHERENT },
+	{ imageUrl: matterMobile, route: DIGITAL_MATTER },
+	{ imageUrl: ecologiesMobile, route: ECOLOGIES_OF_CODE },
+	{ imageUrl: artificeMobile, route: ARTIFICE },
+	{ imageUrl: raveMobile, route: RAVE },
+	{ imageUrl: orbMobile, route: ORB },
+	{ imageUrl: nonplacesMobile, route: NON_PLACES },
+	{ imageUrl: operatorMobile, route: IN_CONVERSATION },
+	{ imageUrl: interwinedMobile, route: INTERTWINED }
 ];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,60 +3,91 @@
 	import HomeIcon from '$lib/elements/HomeIcon/HomeIcon.svelte';
 	import HomeImgRectV from '$lib/elements/HomeImgRectV/HomeImgRectV.svelte';
 	import HomeImgSquare from '$lib/elements/HomeImgSquare/HomeImgSquare.svelte';
-	import { homeImages } from '../data/HomeImgLayout';
+	import { homeImages, homeImagesMobile } from '../data/HomeImgLayout';
 </script>
 
 <div class="flex flex-col items-center justify-center relative w-full h-full">
-	<div class="bg-color-black p-2 w-full h-full flex flex-col sm:flex-row gap-2">
+	<div class="bg-color-black p-2 w-full h-full flex flex-col sm:flex-row gap-[14px]">
 		<div class="w-full sm:min-w-[23.125rem] sm:max-w-[370px] sm:w-home-content-width">
 			<div class="static sm:fixed h-full width-full max-w-[inherit]">
 				<HomeMenu />
 			</div>
 		</div>
 
-		<div class="flex flex-col gap-2">
-			<div class="flex flex-col gap-2 w-full h-full">
+		<div class="flex-col gap-[14px] hidden sm:flex">
+			<div class="flex flex-row w-full gap-[14px]">
+				<div class="flex flex-col gap-[14px] w-1/2">
+					<a href={homeImages[0].route} class="w-full">
+						<HomeImgSquare image={homeImages[0].imageUrl} />
+					</a>
+					<a href={homeImages[1].route} class="w-full">
+						<HomeImgSquare image={homeImages[1].imageUrl} />
+					</a>
+				</div>
+				<div class="w-1/2">
+					<a href={homeImages[2].route}>
+						<HomeImgRectV image={homeImages[2].imageUrl} />
+					</a>
+				</div>
+			</div>
+
+			<div class="w-full flex gap-[14px] flex-col">
+				<div class="flex flex-col sm:flex-row gap-[14px] w-full">
+					<a href={homeImages[3].route} class="w-full">
+						<HomeImgSquare image={homeImages[3].imageUrl} />
+					</a>
+					<a href={homeImages[4].route} class="w-full">
+						<HomeImgSquare image={homeImages[4].imageUrl} />
+					</a>
+				</div>
 				<a href={homeImages[5].route}>
 					<HomeImgRectV image={homeImages[5].imageUrl} />
 				</a>
 			</div>
-			<div class="w-full flex gap-2 flex-col">
-				<div class="flex flex-col sm:flex-row gap-2 w-full">
-					<a href={homeImages[0].route} class="w-full sm:w-1/2">
-						<HomeImgSquare image={homeImages[0].imageUrl} />
+
+			<div class="w-full flex gap-[14px] flex-col">
+				<div class="flex flex-col sm:flex-row gap-[14px] w-full">
+					<a href={homeImages[6].route} class="w-full">
+						<HomeImgSquare image={homeImages[6].imageUrl} />
 					</a>
-					<a href={homeImages[1].route} class="w-full sm:w-1/2">
-						<HomeImgSquare image={homeImages[1].imageUrl} />
+					<a href={homeImages[7].route} class="w-full">
+						<HomeImgSquare image={homeImages[7].imageUrl} />
 					</a>
 				</div>
-				<a href={homeImages[2].route}>
-					<HomeImgRectV image={homeImages[2].imageUrl} />
+				<a href={homeImages[8].route}>
+					<HomeImgRectV image={homeImages[8].imageUrl} />
 				</a>
 			</div>
+		</div>
 
-			<div class="w-full h-full flex gap-2">
-				<a href={homeImages[3].route} class="w-1/2">
-					<HomeImgRectV image={homeImages[3].imageUrl} />
-				</a>
-				<a href={homeImages[4].route} class="w-1/2">
-					<HomeImgRectV image={homeImages[4].imageUrl} />
-				</a>
-			</div>
-
-			<div class="flex flex-col sm:flex-row gap-2 w-full">
-				<a href={homeImages[6].route} class="w-full sm:w-1/2">
-					<HomeImgSquare image={homeImages[6].imageUrl} />
-				</a>
-				<a href={homeImages[7].route} class="w-full sm:w-1/2">
-					<HomeImgSquare image={homeImages[7].imageUrl} />
-				</a>
-			</div>
-
-			<div class="flex flex-col sm:flex-row gap-2 w-full">
-				<a href={homeImages[8].route} class="w-full sm:w-1/2">
-					<HomeImgSquare image={homeImages[8].imageUrl} />
-				</a>
-			</div>
+		<div class="flex flex-col gap-[10px] sm:hidden">
+			<a href={homeImagesMobile[0].route} class="w-full">
+				<HomeImgSquare image={homeImagesMobile[0].imageUrl} />
+			</a>
+			<a href={homeImagesMobile[1].route} class="w-full">
+				<HomeImgSquare image={homeImagesMobile[1].imageUrl} />
+			</a>
+			<a href={homeImagesMobile[2].route}>
+				<HomeImgRectV image={homeImagesMobile[2].imageUrl} />
+			</a>
+			<a href={homeImagesMobile[3].route} class="w-full">
+				<HomeImgSquare image={homeImagesMobile[3].imageUrl} />
+			</a>
+			<a href={homeImagesMobile[4].route} class="w-full">
+				<HomeImgSquare image={homeImagesMobile[4].imageUrl} />
+			</a>
+			<a href={homeImagesMobile[5].route}>
+				<HomeImgRectV image={homeImagesMobile[5].imageUrl} />
+			</a>
+			<a href={homeImagesMobile[6].route} class="w-full">
+				<HomeImgSquare image={homeImagesMobile[6].imageUrl} />
+			</a>
+			<a href={homeImagesMobile[7].route} class="w-full">
+				<HomeImgSquare image={homeImagesMobile[7].imageUrl} />
+			</a>
+			<a href={homeImagesMobile[8].route}>
+				<HomeImgRectV image={homeImagesMobile[8].imageUrl} />
+			</a>
 		</div>
 	</div>
 
