a229bd43ec3ac730f18752a988f3c0c161b64f5e
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-11-04 14:14:37 -0300

    feat: :sparkles: Fix home logo

--- src/lib/elements/HomeLogo/HomeLogo.svelte
+++ src/lib/elements/HomeLogo/HomeLogo.svelte
@@ -17,11 +17,11 @@
 <button
 	style={`background: radial-gradient(at ${(m.x * 1000) / width}% ${(m.y * 1000) / height}%, #A6A6A6, #313431)`}
 	on:mousemove={handleMousemove}
-	class="hidden sm:flex flex-col gap-[16px] w-full {height > 900
+	class="hidden sm:flex flex-col gap-[16px] w-full {height > 1100
 		? 'py-[1.875rem]'
 		: 'py-[1rem]'} px-5 sm:px-10 rounded-20 h-full sm:h-fit"
 >
-	{#if height > 900}
+	{#if height > 1100}
 		<img src={logo} alt="ArtDao Logo" class="w-[163px] h-[37.17633px]" />
 		<div class="flex flex-col items-start">
 			<h1 class="text-[18px] font-clash font-medium leading-[20px] tracking-[0.234px] uppercase">
@@ -33,7 +33,7 @@
 		</div>
 		<img src={hammer} alt="hammer logo" class="w-[200px] my-[0.5rem] self-center" />
 	{/if}
-	{#if height < 900}
+	{#if height < 1100}
 		<div class="flex flex-row">
 			<div class="flex flex-col w-full gap-[1rem]">
 				<img src={logo} alt="ArtDao Logo" class="w-[163px] h-[37.17633px]" />
@@ -58,7 +58,7 @@
 <!-- <button
 	style={`background: radial-gradient(at ${(m.x * 1000) / width}% ${(m.y * 1000) / height}%, #A6A6A6, #313431)`}
 	on:mousemove={handleMousemove}
-	class="sm:hidden flex flex-col gap-[16px] w-full {height > 900
+	class="sm:hidden flex flex-col gap-[16px] w-full {height > 1100
 		? 'py-[1.875rem]'
 		: 'py-[1rem]'} px-5 sm:px-10 rounded-20 h-full sm:h-fit justify-center items-center"
 >
