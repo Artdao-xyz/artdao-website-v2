163ee38c5b80ce07637dc7f3b866f542b7e5644e
Pedro Falco <fpfalco@gmail.com>
2025-03-14 12:45:07 -0300

    implement new Home buttons

---
+++ src/lib/components/HomeButtons/HomeButtons.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import type { Project } from '../../../constants/projects';
+    
+    export let projects: Project[];
+    export let selectedIndex: number;
+    
+    function handleClick(index: number) {
+        // selectedIndex = index;
+        document.getElementById((index + 1).toString())?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+</script>
+
+<div class="h-24 sm:w-40 sm:h-screen flex sm:flex-col items-center justify-center sm:justify-between sm:sticky top-0">
+    <div class="h-full flex flex-row sm:flex-col items-center gap-4 overflow-x-auto sm:overflow-x-hidden sm:overflow-y-auto px-4 pb-4 pt-2 sm:py-8 
+        [mask-image:linear-gradient(to_right,transparent,black_4rem,black_calc(100%-4rem),transparent)] 
+        sm:[mask-image:linear-gradient(to_bottom,transparent,black_4rem,black_calc(100%-4rem),transparent)]">
+        {#each projects as {thumbnailPath}, index}
+            <button 
+                class="flex-shrink-0 h-full sm:h-auto sm:w-full aspect-square rounded-128 overflow-hidden {selectedIndex === index ? 'border-2 sm:border-4 border-color-white' : ''}"
+                on:click={() => handleClick(index)}
+                role="radio"
+                aria-checked={selectedIndex === index}
+            >
+                <img 
+                    draggable="false"
+                    class="w-full h-full object-cover" 
+                    src={thumbnailPath} 
+                    alt=""
+                >
+            </button>
+        {/each}
+    </div>
+</div> 
