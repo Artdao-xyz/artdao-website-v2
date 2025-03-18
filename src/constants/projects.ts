77b5488f4d2096ef73569745fa86f42c66cc3a58
Pedro Falco <fpfalco@gmail.com>
2025-03-18 15:21:53 -0300

    add page paths

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,7 +4,7 @@ export interface Project {
     description: string;
     thumbnailPath: string;
     buttonPath: string;
-    ref: string;
+    pagePath: string;
 }
 
 export const projects: Project[] = [
@@ -14,7 +14,7 @@ export const projects: Project[] = [
                 description: "“Digital mediums are like flying and physical mediums is like walking. It’s that kind of freedom.”",
                 thumbnailPath: "media/home/thumbnails/01-home-digital-matter.png",
                 buttonPath: "media/home/buttons/01-button-digital-matter.png",
-                ref: "01"
+                pagePath: "/digital-matter",
         },
         {
                 title: "Ecologies Of Code",
@@ -22,7 +22,7 @@ export const projects: Project[] = [
                 description: "“Water has a fascinating duality it's both a real-world element and a powerful symbol in our collective imagination.”",
                 thumbnailPath: "media/home/thumbnails/02-home-ecologies-of-code.png",
                 buttonPath: "media/home/buttons/02-button-ecologies-of-code.png",
-                ref: "02"
+                pagePath: "/ecologies-of-code",
         },
         {
                 title: "Inherent Instability",
@@ -30,7 +30,7 @@ export const projects: Project[] = [
                 description: "“Every time I work with this equipment, something new happens—endless ways to connect each machine, it’s like creating a new network.”",
                 thumbnailPath: "media/home/thumbnails/03-home-inherent-instability.png",
                 buttonPath: "media/home/buttons/03-button-inherent-instability.png",
-                ref: "03"
+                pagePath: "/inherent-instability",
         },
         {
                 title: "Artifice",
@@ -38,7 +38,7 @@ export const projects: Project[] = [
                 description: "“As the boundary between artificial and real becomes increasingly blurred - technology leads us into deeper confrontations with the idea of a self that itself is born from fiction.”",
                 thumbnailPath: "media/home/thumbnails/04-home-artifice.png",
                 buttonPath: "media/home/buttons/04-button-artifice.png",
-                ref: "04"
+                pagePath: "/artifice",
         },
         {
                 title: "Conversation with Operator",
@@ -46,7 +46,7 @@ export const projects: Project[] = [
                 description: "“Jeff Davis sat down for an intimate conversation with the artistic duo Operator: Ania Catherine and Dejha exploring the creative process around their 3 act project Human Unreadable.”",
                 thumbnailPath: "media/home/thumbnails/05-home-conversation-with-operator.png",
                 buttonPath: "media/home/buttons/05-button-conversation-with-operator.png",
-                ref: "05"
+                pagePath: "#",
         },
         {
                 title: "Non Places",
@@ -54,7 +54,7 @@ export const projects: Project[] = [
                 description: "In the age of digital code, binary partitions replace geographic coordinates — erasing distinctions between original and copy while transforming from specific location to sites of manifestation.",
                 thumbnailPath: "media/home/thumbnails/06-home-non-places.png",
                 buttonPath: "media/home/buttons/06-button-non-places.png",
-                ref: "06"
+                pagePath: "/non-places",
         },
         {
                 title: "Intertwined",
@@ -62,7 +62,7 @@ export const projects: Project[] = [
                 description: "By embracing the quirks and essence of code rather than just its output, digital artists enter a provocative dance of co-creation that questions who's really in control.",
                 thumbnailPath: "media/home/thumbnails/07-home-intertwined.png",
                 buttonPath: "media/home/buttons/07-button-intertwined.png",
-                ref: "07"
+                pagePath: "/intertwined",
         },
         {
                 title: "RAVE",
@@ -70,7 +70,7 @@ export const projects: Project[] = [
                 description: "As technology infiltrates the once-analog world of underground rave culture, it creates a paradoxical evolution—expanding global connection while transforming the aesthetic language of resistance.",
                 thumbnailPath: "media/home/thumbnails/08-home-rave.png",
                 buttonPath: "media/home/buttons/08-button-rave.png",
-                ref: "08"
+                pagePath: "/rave",
         },
         {
                 title: "O.R.B",
@@ -78,7 +78,7 @@ export const projects: Project[] = [
                 description: "This self-declared 'reunion' deploys institutional parody as creative strategy—simultaneously celebrating and subverting traditional art structures while amplifying Brazilian digital creators on the global stage.",
                 thumbnailPath: "media/home/thumbnails/09-home-orb.png",
                 buttonPath: "media/home/buttons/09-button-orb.png",
-                ref: "09"
+                pagePath: "/orb",
         },
         {
                 title: "Body Frontier",
@@ -86,7 +86,7 @@ export const projects: Project[] = [
                 description: "“3D allows me to create worlds and characters, and to explore my identity through avatars that slowly transform, mutating between different elements.”",
                 thumbnailPath: "media/home/thumbnails/10-home-body-frontier.png",
                 buttonPath: "media/home/buttons/10-button-body-frontier.png",
-                ref: "10"
+                pagePath: "#",
         },
         {
                 title: "What's On Your Mind?",
@@ -94,7 +94,7 @@ export const projects: Project[] = [
                 description: "“Despite the many tools and possibilities there's a responsibility to use what cannot be easily translated into algorithms challenging the limitations of the platform while embracing its possibilities.”",
                 thumbnailPath: "media/home/thumbnails/11-home-whats-on-your-mind.png",
                 buttonPath: "media/home/buttons/11-button-whats-on-your-mind.png",
-                ref: "11"
+                pagePath: "#",
         },
         {
                 title: "Memetic Rubble",
@@ -102,7 +102,7 @@ export const projects: Project[] = [
                 description: "“Taking memes and processing them through AI, transforming them into something new is what makes memes exceptional. They lack ownership — inherently meant to be shared, remixed, and transformed by anyone.”",
                 thumbnailPath: "media/home/thumbnails/12-home-memetic-rubble.png",
                 buttonPath: "media/home/buttons/12-button-memetic-rubble.png",
-                ref: "12"
+                pagePath: "#",
         },
         {
                 title: "Subconscious Media",
@@ -110,7 +110,7 @@ export const projects: Project[] = [
                 description: "“More tools gives the possibility to better express what you don't quite know what it is. Even if you are unsure of the path, the important thing is not to lose what guides you, the essential, what really matters.”",
                 thumbnailPath: "media/home/thumbnails/13-home-subconscious-media.png",
                 buttonPath: "media/home/buttons/13-button-subconscious-media.png",
-                ref: "13"
+                pagePath: "#",
         },
         {
                 title: "Digital Archaeology",
@@ -118,6 +118,6 @@ export const projects: Project[] = [
                 description: "We are constantly bombarded by new media — Yet, I remain influenced by the aesthetics of the past. Limitations offer a form of comfort when creating art.",
                 thumbnailPath: "media/home/thumbnails/14-home-digital-archaeology.png",
                 buttonPath: "media/home/buttons/14-button-digital-archaeology.png",
-                ref: "14"
+                pagePath: "#",
         }
 ];
