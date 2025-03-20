7902dc44d7ab96809d478ab8c46ceedfb700e8cb
Pedro Falco <fpfalco@gmail.com>
2025-03-20 11:20:19 -0300

    make loading animation smaller

--- src/lib/components/Loading/Loading.svelte
+++ src/lib/components/Loading/Loading.svelte
@@ -7,6 +7,6 @@
 <div
 	class="bg-white absolute left-0 top-0 w-full h-full flex flex-col justify-center items-center gap-4 z-50"
 >
-	<div class="h-[25%]"><img class="h-full" src={loading} alt="Loading gif" /></div>
+	<div class="h-[20%]"><img class="h-full" src={loading} alt="Loading gif" /></div>
 	<progress class="invisible" value={$progress} max="100"></progress>
 </div>
