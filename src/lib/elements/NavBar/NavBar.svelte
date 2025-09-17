d6f8b9d498edd2cb356bfae80b2bcf270efb9ab8
pedrofalco <fpfalco@gmail.com>
2025-09-17 11:19:20 -0300

    fix: hide NavBar desktop version on mobile
    - Add hidden sm:flex classes to desktop NavBar to prevent it from showing on mobile
    - Keep mobile NavBar (sm:hidden) visible for mobile devices
    - Ensure proper responsive behavior: desktop version only on desktop, mobile version only on mobile

--- src/lib/elements/NavBar/NavBar.svelte
+++ src/lib/elements/NavBar/NavBar.svelte
@@ -51,7 +51,7 @@
 	<div
 		class="{hideNav
 			? 'justify-between'
-			: ''} w-100dvw rounded-[6.25rem] h-[1rem] flex flex-row items-center z-40 sticky top-[2.88%] mx-[1.625rem] gap-2.5"
+			: ''} w-100dvw rounded-[6.25rem] h-[1rem] flex flex-row items-center z-40 sticky top-[2.88%] mx-[1.625rem] gap-2.5 hidden sm:flex"
 	>
 		<a href={'/'}>
 			<div
