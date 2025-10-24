15426f25098bbe666c789680fdeda5246d31cc73
pedrofalco <fpfalco@gmail.com>
2025-10-24 17:48:10 -0300

    feat: update CTA to link to bertie@artdao.xyz email
    - Change 'Get in touch' CTA href from empty to mailto:bertie@artdao.xyz
    - Users can now click to open their email client with Bertie's email pre-filled

--- src/routes/studio/+page.svelte
+++ src/routes/studio/+page.svelte
@@ -14,6 +14,6 @@
 <StudioController/>
 
 <div class="bg-black relative lg:absolute lg:bottom-0 lg:left-1/2 lg:transform lg:-translate-x-1/2 w-full px-4 flex justify-between items-center gap-2 p-4">
-        <a href="" class="text-white font-bold font-robotoMono leading-snug tracking-wide space-x-1"><span>Get in touch</span><ArrowUpRightIcon class="w-4 h-4 inline-block" /></a>
+        <a href="mailto:bertie@artdao.xyz" class="text-white font-bold font-robotoMono leading-snug tracking-wide space-x-1"><span>Get in touch</span><ArrowUpRightIcon class="w-4 h-4 inline-block" /></a>
         <span class="text-white text-[10px] font-normal font-robotoMono leading-3 tracking-tight">artdao © 2025</span>
 </div>
