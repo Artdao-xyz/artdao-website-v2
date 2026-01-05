0339c4af84241e089c60d8ddad282e029c0f09ff
pedrofalco <fpfalco@gmail.com>
2026-01-05 14:25:03 -0300

    update copy

--- src/data/Projects/DigitalArchaeology/ProjectIntro.ts
+++ src/data/Projects/DigitalArchaeology/ProjectIntro.ts
@@ -1,9 +1,10 @@
 export const digitalArchaeologyIntro = {
 	name: 'digital archaeology',
 	description: [
-		'The return to retro digital aesthetics operates as a form of resistance—pushing back against the sleek refinement and visual excess of contemporary digital culture. By reviving obsolete formats and interfaces, artists engage in a kind of digital archaeology, embracing technical limitations as both critique and method.',
-		'These digital archaeologists reveal how constraint itself can become a tool of liberation—an antidote to the glut of frictionless, overproduced imagery saturating our screens. This practice introduces a temporal disjunction, where outmoded technologies collide with present-day concerns, producing work that interrogates our evolving relationship with digital media.',
-		'It becomes a space where historical and contemporary visual languages coalesce and destabilize one another. The tension between past aesthetics and present capabilities exposes the ideologies embedded in our digital tools. In this light, innovation may not mean overcoming constraints, but rather critical engaging with digital limitations.'
+		'The return to retro digital aesthetics operates as a form of resistance—pushing back against the sleek refinement and visual excess of contemporary digital culture.',
+		'By reviving obsolete formats and interfaces, artists engage in a kind of digital archaeology, embracing technical limitations as both critique and method. These digital archaeologists reveal how constraint itself can become a tool of liberation—an antidote to the glut of frictionless, overproduced imagery saturating our screens.',
+		'This practice introduces a temporal disjunction, where outmoded technologies collide with present-day concerns, producing work that interrogates our evolving relationship with digital media. It becomes a space where historical and contemporary visual languages coalesce and destabilize one another.',
+		'The tension between past aesthetics and present capabilities exposes the ideologies embedded in our digital tools. In this light, innovation may not mean overcoming constraints, but rather critical engaging with digital limitations.'
 	],
 	bgImage: '/media/digital-archaeology/main-cover.webp',
 	bgImageMobile: '/media/digital-archaeology/main-cover.webp'
