f497bca6cbf140a2ce0407307a3cc919d92bd581
pedrofalco <fpfalco@gmail.com>
2026-02-06 12:56:28 -0300

    Update title format with line break and nowrap

--- src/data/Projects/OneBodyAThousandWorlds/ProjectIntro.ts
+++ src/data/Projects/OneBodyAThousandWorlds/ProjectIntro.ts
@@ -1,7 +1,7 @@
 import type { IProject } from '$lib/components/ProjectIntro/interfaces';
 
 export const oneBodyAThousandWorldsIntro: IProject = {
-	name: 'one body, a thousand worlds',
+	name: 'one body,<br><span style="white-space: nowrap;">a thousand worlds</span>',
 	description: [
 		'What does it mean to build an entire creative practice around a single character? In the hands of certain artists, such figures are far more than illustrated avatars—they operate as symbolic nodes, anchoring entire ecosystems of meaning. From these characters, entire worlds emerge: affective atmospheres, visual languages, narrative arcs, and cosmological logics. Rather than depicting isolated scenes, these artists create expansive personal mythologies—less like a traditional comic or storyboard, and more akin to a poetic saga or open-world video game. The logic is less linear than intuitive, built through recursive gestures and emotional cues.',
 		"This approach is especially compelling in web3, where identity, authorship, and storytelling evolve across distributed networks. That's why we spoke with artists like Turro and Salawaki, whose iconic characters serve as central conduits for world-building. Through these conversations, we wanted to better understand what it means to orbit a singular figure—how characters become containers for memory, mood, politics, and possibility."
