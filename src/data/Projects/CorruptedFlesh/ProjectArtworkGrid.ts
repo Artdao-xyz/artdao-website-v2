4ff0e78af0432439e0cc252d138b01c11ae09496
pedrofalco <fpfalco@gmail.com>
2026-02-13 14:23:26 -0300

    Add mobile version for Corrupted Flesh artwork grids

--- src/data/Projects/CorruptedFlesh/ProjectArtworkGrid.ts
+++ src/data/Projects/CorruptedFlesh/ProjectArtworkGrid.ts
@@ -1,4 +1,4 @@
-import type { IGalleryImage } from '$lib/elements/ArtworkContainer/interfaces';
+import type { IGalleryImage, IGalleryImageMobile } from '$lib/elements/ArtworkContainer/interfaces';
 
 export const corruptedFleshArtworkGrid1: IGalleryImage[] = [
 	{
@@ -30,6 +30,45 @@ export const corruptedFleshArtworkGrid1: IGalleryImage[] = [
 		image: '/media/corrupted-flesh/artworks-grid/artworks-grid-6.webp',
 		name: '',
 		description: ''
+	}
+];
+
+export const corruptedFleshArtworkGrid1Mobile: IGalleryImageMobile[] = [
+	{
+		src: '/media/corrupted-flesh/artworks-grid/artworks-grid-1.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 1'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid/artworks-grid-2.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 2'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid/artworks-grid-3.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 3'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid/artworks-grid-4.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 4'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid/artworks-grid-5.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 5'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid/artworks-grid-6.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 6'
 	}
 ];
 
@@ -66,6 +105,45 @@ export const corruptedFleshArtworkGrid2: IGalleryImage[] = [
 	}
 ];
 
+export const corruptedFleshArtworkGrid2Mobile: IGalleryImageMobile[] = [
+	{
+		src: '/media/corrupted-flesh/artworks-grid-2/artworks-grid-1.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 7'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-2/artworks-grid-2.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 8'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-2/artworks-grid-3.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 9'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-2/artworks-grid-4.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 10'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-2/artworks-grid-5.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 11'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-2/artworks-grid-6.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 12'
+	}
+];
+
 export const corruptedFleshArtworkGrid3: IGalleryImage[] = [
 	{
 		image: '/media/corrupted-flesh/artworks-grid-3/artworks-grid-1.webp',
@@ -99,3 +177,41 @@ export const corruptedFleshArtworkGrid3: IGalleryImage[] = [
 	}
 ];
 
+export const corruptedFleshArtworkGrid3Mobile: IGalleryImageMobile[] = [
+	{
+		src: '/media/corrupted-flesh/artworks-grid-3/artworks-grid-1.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 13'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-3/artworks-grid-2.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 14'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-3/artworks-grid-3.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 15'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-3/artworks-grid-4.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 16'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-3/artworks-grid-5.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 17'
+	},
+	{
+		src: '/media/corrupted-flesh/artworks-grid-3/artworks-grid-6.webp',
+		name: '',
+		description: '',
+		alt: 'Corrupted Flesh Artwork 18'
+	}
+];
