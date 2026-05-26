4b810d06f4ae236243edec9b8bf1324d87c73964
pedrofalco <fpfalco@gmail.com>
2026-05-26 10:16:58 -0300

    hotfix: update syntetic senses images

--- src/data/Projects/SyntheticSenses/ProjectAboutDropdown.ts
+++ src/data/Projects/SyntheticSenses/ProjectAboutDropdown.ts
@@ -2,20 +2,28 @@ import type { IAboutDropdown } from '$lib/elements/AboutDropdown/interfaces';
 
 const REFIK_ANADOL_IMAGE_CREDIT = 'Images Courtesy of Refik Anadol Studio';
 
+const syntheticSensesParagRefikAnadolImages = new Set([
+	'/media/synthetic-senses/WDCHDreams-Keyword_select.width-1600.format-webp.webp',
+	'/media/synthetic-senses/treemap-2048x1152.webp'
+]);
+
 export const syntheticSensesParagGridImages = [
 	'/media/synthetic-senses/WDCHDreams-Keyword_select.width-1600.format-webp.webp',
-	'/media/synthetic-senses/STILL_27-2048x1353.webp',
-	'/media/synthetic-senses/230808_LA-Phil-Concept-Presentation-1-2048x1152.webp',
 	'/media/synthetic-senses/treemap-2048x1152.webp',
-	'/media/synthetic-senses/crop2-1.webp',
-	'/media/synthetic-senses/STILL_27-scaled.webp'
+	'/media/synthetic-senses/Artboard%201.webp',
+	'/media/synthetic-senses/Artboard%203_1.webp',
+	'/media/synthetic-senses/Artboard%209.webp',
+	'/media/synthetic-senses/Overhead1%202.webp',
+	'/media/synthetic-senses/image.webp'
 ];
 
 export const syntheticSensesParagDropdownItems: IAboutDropdown[] =
-	syntheticSensesParagGridImages.map((image, index) => ({
-		name: index < 2 ? REFIK_ANADOL_IMAGE_CREDIT : '',
+	syntheticSensesParagGridImages.map((image) => ({
+		name: syntheticSensesParagRefikAnadolImages.has(image)
+			? REFIK_ANADOL_IMAGE_CREDIT
+			: '',
 		image,
-		normalCase: index < 2
+		normalCase: syntheticSensesParagRefikAnadolImages.has(image)
 	}));
 
 export const syntheticSensesAuroraGridImages = [
